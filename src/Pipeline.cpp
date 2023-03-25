3ca5e91f7d0758dfd963eaa3dcba1d9d1f74ac72
Marcel <marceleenah@gmail.com>
2023-03-25 18:25:02 +0100

    Index rendering

--- src/Pipeline.cpp
+++ src/Pipeline.cpp
@@ -52,29 +52,50 @@ void Pipeline::draw() {
         float r, g, b;
     };
 
-    const Point vertices[] = {
-        { .5f,  .5f, 1.f, 0.f, 0.f},
-        { .5f, -.5f, 0.f, 1.f, 0.f},
-        {-.5f, -.5f, 0.f, 0.f, 1.f},
+    Point vertices[] = {
+        { .5f,  .5f, 1.f, 0.f, 0.f}, // top-right
+        { .5f, -.5f, 0.f, 1.f, 0.f}, // bottom-right
+        {-.5f, -.5f, 0.f, 0.f, 1.f}, // bottom-left
 
-        {-.5f,  .5f, 0.f, 1.f, 0.f},
-        { .5f,  .5f, 1.f, 0.f, 0.f},
-        {-.5f, -.5f, 0.f, 0.f, 1.f}
+        {-.5f,  .5f, 0.f, 1.f, 0.f}, // top-left
+    };
+
+    uint16_t indices[] = {
+        0, 1, 2,
+        3, 0, 2
     };
 
     wrl::ComPtr<ID3D11Buffer> vertexBuffer;
-    D3D11_BUFFER_DESC bufferDescriptor{};
-    bufferDescriptor.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-    bufferDescriptor.Usage = D3D11_USAGE_DEFAULT;
-    bufferDescriptor.CPUAccessFlags = NULL;
-    bufferDescriptor.MiscFlags = NULL;
-    bufferDescriptor.ByteWidth = sizeof(vertices);
-    bufferDescriptor.StructureByteStride = sizeof(Point);
+    D3D11_BUFFER_DESC vertexBufferDescriptor{};
+    vertexBufferDescriptor.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    vertexBufferDescriptor.Usage = D3D11_USAGE_DEFAULT;
+    vertexBufferDescriptor.CPUAccessFlags = NULL;
+    vertexBufferDescriptor.MiscFlags = NULL;
+    vertexBufferDescriptor.ByteWidth = sizeof(vertices);
+    vertexBufferDescriptor.StructureByteStride = sizeof(Point);
 
-    D3D11_SUBRESOURCE_DATA subresourceData{};
-    subresourceData.pSysMem = vertices;
+    D3D11_SUBRESOURCE_DATA vertexSubresourceData{};
+    vertexSubresourceData.pSysMem = vertices;
 
-    device->CreateBuffer(&bufferDescriptor, &subresourceData, vertexBuffer.GetAddressOf());
+    wrl::ComPtr<ID3D11Buffer> indexBuffer;
+    D3D11_BUFFER_DESC indexBufferDescriptor{};
+    indexBufferDescriptor.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    indexBufferDescriptor.Usage = D3D11_USAGE_DEFAULT;
+    indexBufferDescriptor.CPUAccessFlags = NULL;
+    indexBufferDescriptor.MiscFlags = NULL;
+    indexBufferDescriptor.ByteWidth = sizeof(vertices);
+    indexBufferDescriptor.StructureByteStride = sizeof(uint16_t);
+
+    D3D11_SUBRESOURCE_DATA indexSubresourceData{};
+    indexSubresourceData.pSysMem = indices;
+
+    device->CreateBuffer(&vertexBufferDescriptor, &vertexSubresourceData, vertexBuffer.GetAddressOf());
+    device->CreateBuffer(&indexBufferDescriptor, &indexSubresourceData, indexBuffer.GetAddressOf());
+
+    // Bind index buffer
+    deviceContext->IASetIndexBuffer(indexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0);
+
+    // Bind vertex buffer
     const uint stride = sizeof(Point);
     const uint offset = 0;
     deviceContext->IASetVertexBuffers(0, 1, vertexBuffer.GetAddressOf(), &stride, &offset);
@@ -111,5 +132,5 @@ void Pipeline::draw() {
     deviceContext->OMSetRenderTargets(1, renderTarget.GetAddressOf(), nullptr);
     deviceContext->RSSetViewports(1, &viewport);
 
-    deviceContext->Draw(std::size(vertices), 0);
+    deviceContext->DrawIndexed(std::size(indices), 0, 0);
 }
